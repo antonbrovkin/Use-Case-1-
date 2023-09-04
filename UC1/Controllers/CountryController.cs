d22faa8e1710214b4da0ddda33baec9ab4a03f12
antonbrovkin <antbrov@gmail.com>
2023-09-04 12:57:40 +0300

    UC#1 Create sort function by name

--- UC1/Controllers/CountryController.cs
+++ UC1/Controllers/CountryController.cs
@@ -18,7 +18,7 @@ namespace UC1.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get(int? population, string commonCountryName = "")
+        public async Task<IActionResult> Get(int? population, string commonCountryName = "", string commonCountryNameSorting = "")
         {
             List<Country> countries = await GetCountriesAsync();
 
@@ -32,6 +32,11 @@ namespace UC1.Controllers
                 countries = FilterCountriesByPopulation(countries, population.Value * 1000000).ToList();
             }
 
+            if (!string.IsNullOrWhiteSpace(commonCountryNameSorting))
+            {
+                countries = SortCountries(countries, commonCountryNameSorting).ToList();
+            }
+
             return Ok(countries);
         }
 
@@ -43,6 +48,22 @@ namespace UC1.Controllers
         public static IEnumerable<Country> FilterCountriesByPopulation(List<Country> countries, long threshold)
         {
             return countries.Where(c => c.Population < threshold);
+        }
+
+        public static IEnumerable<Country> SortCountries(List<Country> countries, string order)
+        {
+            if (order.ToLower() == "ascend")
+            {
+                return countries.OrderBy(c => c.Name.Common);
+            }
+            else if (order.ToLower() == "descend")
+            {
+                return countries.OrderByDescending(c => c.Name.Common);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid order parameter. Use 'ascend' or 'descend'.");
+            }
         }
     }
 }
